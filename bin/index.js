951713a99bb02c302a91521aaa2ca26df4446721
abouthiroppy <git@about-hiroppy.com>
2016-12-27 10:19:41 +0900

    refactor(plugins): delete plugins

--- bin/index.js
+++ bin/index.js
@@ -9,4 +9,10 @@ if (commands[0] === '-v' || commands[0] === '--version') {
   process.exit(0);
 }
 
+if (commands[0] === '--site') {
+  console.log('Plugins: https://babeljs.io/docs/plugins/');
+
+  process.exit(0);
+}
+
 require('../lib');
